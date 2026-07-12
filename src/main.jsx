8657b17fd100ae8ac2353b839b1379455924b6c6
magnolia668 <magnolia668@users.noreply.github.com>
2026-07-12 23:07:54 +0800

    fix: use HashRouter and add _redirects for Cloudflare Pages SPA routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from "react"
+﻿import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { I18nProvider } from "./i18n"
 import "./index.css"
 import App from "./App"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <I18nProvider>
         <App />
       </I18nProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
